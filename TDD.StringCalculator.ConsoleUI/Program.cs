62efab0e17b6a0c4e36963134bcc8a14d2af8294
Mano Cardozo <manasses.cardozo@gmail.com>
2020-08-04 22:05:22 +1200

    initial commit

---
+++ TDD.StringCalculator.ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TDD.StringCalculator.ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
