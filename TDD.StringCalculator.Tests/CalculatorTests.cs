de3c2b1bde0c3b64071ad95a845353060d41d838
Mano Cardozo <manasses.cardozo@gmail.com>
2020-08-06 18:25:29 +1200

    Satisfy requirement 9

--- TDD.StringCalculator.Tests/CalculatorTests.cs
+++ TDD.StringCalculator.Tests/CalculatorTests.cs
@@ -123,6 +123,21 @@ namespace TDD.StringCalculator.Tests
 
         [Theory]
         [InlineData("//[*][%]\n1*2%3", 6)]
+        [InlineData("//[-][;]\n1-2;3,4", 10)]
+        public void Add_WithMultipleCustomDelimiterAddsUpToAnyNumbers_WhenGivenValidString(string numbers, int expected)
+        {
+            // Arrange
+            var sut = new Calculator();
+
+            // Act
+            var result = sut.Add(numbers);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+
+        [Theory]
+        [InlineData("//[*][%]\n1*2%3", 6)]
         [InlineData("//[***][%%]\n1***2,3%%4", 10)]
         public void Add_WithMultipleSizedCustomDelimiterAddsUpToAnyNumbers_WhenGivenValidString(string numbers, int expected)
         {
